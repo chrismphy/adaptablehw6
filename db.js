6971d5969dd92b5bc68e29688c1e76b88fd1738e
chrismphy <49465122+chrismphy@users.noreply.github.com>
2023-10-18 10:13:17 -0400

    db

--- db.js
+++ db.js
@@ -1,9 +1,24 @@
 const knex = require('knex');
 
+let connection;
+
+// Check if the DATABASE_URL environment variable is set (indicative of Heroku environment)
+if (process.env.DATABASE_URL) {
+    connection = process.env.DATABASE_URL;
+} else {
+    connection = {
+        host: 'localhost',
+        user: 'chrismphy',
+        password: 'Rubedo1989',
+        database: 'postgres',
+        port: 5432
+    };
+}
+
 const db = knex({
-  client: 'pg',
-  connection: process.env.DATABASE_URL,
-  searchPath: ['knex', 'public'],
+    client: 'pg',
+    connection: connection,
+    searchPath: ['knex', 'public']
 });
 
 module.exports = db;
