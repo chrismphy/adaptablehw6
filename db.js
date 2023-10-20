62de260d6cb6ce8260da231d8944adf23bf54b8b
chrismphy <49465122+chrismphy@users.noreply.github.com>
2023-10-19 20:18:48 -0400

    db change for sql

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-const knex = require('knex');
+const { Pool } = require('pg');
 
-// Define the database connection configuration
+// Database connection parameters
 const connection = {
     host: 'user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com',
     user: 'chrismphy-main-db-07b32ef7ec26607cb',
@@ -9,10 +9,31 @@ const connection = {
     port: 5432
 };
 
-const db = knex({
-    client: 'pg',
-    connection: connection,
-    searchPath: ['knex', 'public']
-});
+// Create a new Pool instance
+const pool = new Pool(connection);
 
-module.exports = db;
+// SQL command to create a table
+const createTableQuery = `
+CREATE TABLE students (
+    record_id bigint PRIMARY KEY,
+    first_name character varying,
+    last_name character varying,
+    gpa numeric,
+    enrolled boolean,
+    uploaded_at timestamp without time zone
+);
+`;
+
+// Connect to the database and execute the query
+pool.query(createTableQuery)
+  .then(() => {
+    console.log('Table created successfully');
+  })
+  .catch((err) => {
+    console.error('Error creating table:', err);
+  });
+
+// Note: You don't need to call pool.end() here; the pool can be reused for other database operations in your application.
+
+// Export the pool for use in other parts of your application if needed
+module.exports = pool;
